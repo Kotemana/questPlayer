d4ffefcd8b1fa63449a09d507a94a17344ab24f5
Kunio-kun <turskiyan@gmail.com>
2017-04-17 17:02:40 +0300

    Debug

---
+++ questPlayer/Content/Load.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    console.log("document loaded");
+});
+var retrieveMyData = localStorage.getItem('myData');
+var data = JSON.parse(retrieveMyData);
+var url = "/Quest/Question?questId=" + data.questId + "&questionId=" + data.questionId + "&startNew=True";
+if (data.questId || data.questId != null) {
+    var tag = "<a href='" + url + "'> Continue </a>";
+    document.getElementById("Continue").innerHTML = tag;
+}
---
+++ questPlayer/Content/Save.js
@@ -0,0 +1,11 @@
+﻿$(".answer-link").click(function () {
+    alert($(this).data("result"));
+});
+
+var container = $("#viewDataContainer");
+var myData = {
+    questId: container.attr("data-questId"),
+    questionId: container.attr("data-questionId")
+}
+console.log(myData);
+localStorage.setItem('myData', JSON.stringify(myData));
